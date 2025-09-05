af02fb541c1e0d3467928e971e0ae2050dc8a907
Randy Josleyn <randy.josleyn@gmail.com>
2025-09-04 23:48:05 -0600

    hide Compare field on report print

--- src/reports/BalanceSheetReport.vue
+++ src/reports/BalanceSheetReport.vue
@@ -22,16 +22,22 @@
 			<div class="grow flex gap-x-2 items-baseline">
 				<span class="whitespace-nowrap">As at</span>
 				<input type="date" class="bordered-field" v-model.lazy="dt">
-				<span>Compare</span>
-				<div class="relative flex flex-grow items-stretch shadow-sm">
-					<input type="number" min="1" class="bordered-field w-[9.5em] pr-[6em]" v-model.lazy="comparePeriods">
-					<div class="absolute inset-y-0 right-0 flex items-center z-10">
-						<select class="h-full border-0 bg-transparent py-0 pl-2 pr-8 text-gray-900 focus:ring-2 focus:ring-inset focus:ring-emerald-600" v-model="compareUnit">
-							<option value="years">years</option>
-							<option value="months">months</option>
-						</select>
+				<div class="print:hidden flex items-center gap-2">
+					<span>Compare</span>
+					<div class="relative flex flex-grow items-stretch shadow-sm">
+						<input type="number" min="1" class="bordered-field w-[9.5em] pr-[6em]"
+							v-model.lazy="comparePeriods">
+						<div class="absolute inset-y-0 right-0 flex items-center z-10">
+							<select
+								class="h-full border-0 bg-transparent py-0 pl-2 pr-8 text-gray-900 focus:ring-2 focus:ring-inset focus:ring-emerald-600"
+								v-model="compareUnit">
+								<option value="years">years</option>
+								<option value="months">months</option>
+							</select>
+						</div>
 					</div>
 				</div>
+
 				<DynamicReportMenu :report="report" :columns="reportColumns" :subtitle="'As at ' + dt" />
 			</div>
 		</div>
@@ -41,7 +47,8 @@
 					<ExclamationCircleIcon class="h-5 w-5 text-red-400" />
 				</div>
 				<div class="ml-3 flex-1">
-					<p class="text-sm text-red-700">Total assets do not equal total liabilities and equity. This may occur if not all accounts have been classified in the chart of accounts.</p>
+					<p class="text-sm text-red-700">Total assets do not equal total liabilities and equity. This may
+						occur if not all accounts have been classified in the chart of accounts.</p>
 				</div>
 			</div>
 		</div>
@@ -49,88 +56,88 @@
 </template>
 
 <script setup lang="ts">
-	import dayjs from 'dayjs';
-	import { invoke } from '@tauri-apps/api/core';
-	import { computed, ref, watch } from 'vue';
-	
-	import { ExclamationCircleIcon } from '@heroicons/vue/20/solid';
-	
-	import { DynamicReport, Row } from './base.ts';
-	import { db } from '../db.ts';
-	import DynamicReportComponent from '../components/DynamicReportComponent.vue';
-	import DynamicReportMenu from '../components/DynamicReportMenu.vue';
-	
-	const report = ref(null as DynamicReport | null);
-	const reportColumns = ref([] as string[]);
-	
-	const dt = ref(null as string | null);
-	const comparePeriods = ref(1);
-	const compareUnit = ref('years');
-	
-	async function load() {
-		await db.load();
-		
-		dt.value = db.metadata.eofy_date;
-		
-		await updateReport();
-		
-		// Update report when dates etc. changed
-		// We initialise the watcher here only after dt is initialised above
-		watch([dt, comparePeriods, compareUnit], updateReport);
-	}
-	load();
-	
-	async function updateReport() {
-		const reportDates = [];
-		let newReportColumns = [];
-		
-		for (let i = 0; i < comparePeriods.value; i++) {
-			let thisReportDt;
-			
-			// Get period end date
-			if (compareUnit.value === 'years') {
-				thisReportDt = dayjs(dt.value!).subtract(i, 'year');
-				newReportColumns.push(thisReportDt.format('YYYY'));
-			} else if (compareUnit.value === 'months') {
-				if (dayjs(dt.value!).add(1, 'day').isSame(dayjs(dt.value!).set('date', 1).add(1, 'month'))) {
-					// If dt is the end of a calendar month, then fix each prior dt to be the end of the calendar month
-					thisReportDt = dayjs(dt.value!).subtract(i, 'month').set('date', 1).add(1, 'month').subtract(1, 'day');
-				} else {
-					thisReportDt = dayjs(dt.value!).subtract(i, 'month');
-				}
-				newReportColumns.push(thisReportDt.format('YYYY-MM'));
+import dayjs from 'dayjs';
+import { invoke } from '@tauri-apps/api/core';
+import { computed, ref, watch } from 'vue';
+
+import { ExclamationCircleIcon } from '@heroicons/vue/20/solid';
+
+import { DynamicReport, Row } from './base.ts';
+import { db } from '../db.ts';
+import DynamicReportComponent from '../components/DynamicReportComponent.vue';
+import DynamicReportMenu from '../components/DynamicReportMenu.vue';
+
+const report = ref(null as DynamicReport | null);
+const reportColumns = ref([] as string[]);
+
+const dt = ref(null as string | null);
+const comparePeriods = ref(1);
+const compareUnit = ref('years');
+
+async function load() {
+	await db.load();
+
+	dt.value = db.metadata.eofy_date;
+
+	await updateReport();
+
+	// Update report when dates etc. changed
+	// We initialise the watcher here only after dt is initialised above
+	watch([dt, comparePeriods, compareUnit], updateReport);
+}
+load();
+
+async function updateReport() {
+	const reportDates = [];
+	let newReportColumns = [];
+
+	for (let i = 0; i < comparePeriods.value; i++) {
+		let thisReportDt;
+
+		// Get period end date
+		if (compareUnit.value === 'years') {
+			thisReportDt = dayjs(dt.value!).subtract(i, 'year');
+			newReportColumns.push(thisReportDt.format('YYYY'));
+		} else if (compareUnit.value === 'months') {
+			if (dayjs(dt.value!).add(1, 'day').isSame(dayjs(dt.value!).set('date', 1).add(1, 'month'))) {
+				// If dt is the end of a calendar month, then fix each prior dt to be the end of the calendar month
+				thisReportDt = dayjs(dt.value!).subtract(i, 'month').set('date', 1).add(1, 'month').subtract(1, 'day');
 			} else {
-				throw new Error('Unexpected compareUnit');
+				thisReportDt = dayjs(dt.value!).subtract(i, 'month');
 			}
-			
-			reportDates.push(thisReportDt.format('YYYY-MM-DD'));
+			newReportColumns.push(thisReportDt.format('YYYY-MM'));
+		} else {
+			throw new Error('Unexpected compareUnit');
 		}
-		
-		if (comparePeriods.value === 1) {
-			// Override column headers if only one column
-			newReportColumns = ['$'];
-		}
-		
-		report.value = DynamicReport.fromJSON(await invoke('get_balance_sheet', { dates: reportDates }));
-		reportColumns.value = newReportColumns;  // Wait until report available to update this
+
+		reportDates.push(thisReportDt.format('YYYY-MM-DD'));
 	}
-	
-	const doesBalance = computed(function() {
-		if (report.value === null) {
-			return true;
+
+	if (comparePeriods.value === 1) {
+		// Override column headers if only one column
+		newReportColumns = ['$'];
+	}
+
+	report.value = DynamicReport.fromJSON(await invoke('get_balance_sheet', { dates: reportDates }));
+	reportColumns.value = newReportColumns;  // Wait until report available to update this
+}
+
+const doesBalance = computed(function () {
+	if (report.value === null) {
+		return true;
+	}
+
+	const totalAssets = (report.value.byId('total_assets') as { Row: Row }).Row.quantity;
+	const totalLiabilities = (report.value.byId('total_liabilities') as { Row: Row }).Row.quantity;
+	const totalEquity = (report.value.byId('total_equity') as { Row: Row }).Row.quantity;
+
+	let doesBalance = true;
+	for (let column = 0; column < report.value.columns.length; column++) {
+		if (totalAssets[column] !== totalLiabilities[column] + totalEquity[column]) {
+			doesBalance = false;
+			break;
 		}
-		
-		const totalAssets = (report.value.byId('total_assets') as { Row: Row }).Row.quantity;
-		const totalLiabilities = (report.value.byId('total_liabilities') as { Row: Row }).Row.quantity;
-		const totalEquity = (report.value.byId('total_equity') as { Row: Row }).Row.quantity;
-		
-		let doesBalance = true;
-		for (let column = 0; column < report.value.columns.length; column++) {
-			if (totalAssets[column] !== totalLiabilities[column] + totalEquity[column]) {
-				doesBalance = false;
-				break;
-			}
-		}
-		return doesBalance;
-	});
+	}
+	return doesBalance;
+});
 </script>
--- src/reports/IncomeStatementReport.vue
+++ src/reports/IncomeStatementReport.vue
@@ -22,14 +22,19 @@
 			<input type="date" class="bordered-field" v-model.lazy="dtStart">
 			<span>to</span>
 			<input type="date" class="bordered-field" v-model.lazy="dt">
-			<span>Compare</span>
-			<div class="relative flex flex-grow items-stretch shadow-sm">
-				<input type="number" min="1" class="bordered-field w-[9.5em] pr-[6em]" v-model.lazy="comparePeriods">
-				<div class="absolute inset-y-0 right-0 flex items-center z-10">
-					<select class="h-full border-0 bg-transparent py-0 pl-2 pr-8 text-gray-900 focus:ring-2 focus:ring-inset focus:ring-emerald-600" v-model="compareUnit" @change="onCompareUnitChange">
-						<option value="years">years</option>
-						<option value="months">months</option>
-					</select>
+			<div class="print:hidden flex items-center gap-2">
+				<span>Compare</span>
+				<div class="relative flex flex-grow items-stretch shadow-sm">
+					<input type="number" min="1" class="bordered-field w-[9.5em] pr-[6em]"
+						v-model.lazy="comparePeriods">
+					<div class="absolute inset-y-0 right-0 flex items-center z-10">
+						<select
+							class="h-full border-0 bg-transparent py-0 pl-2 pr-8 text-gray-900 focus:ring-2 focus:ring-inset focus:ring-emerald-600"
+							v-model="compareUnit">
+							<option value="years">years</option>
+							<option value="months">months</option>
+						</select>
+					</div>
 				</div>
 			</div>
 			<DynamicReportMenu :report="report" :columns="reportColumns" :subtitle="dtStart + ' to ' + dt" />
@@ -38,98 +43,98 @@
 </template>
 
 <script setup lang="ts">
-	import dayjs from 'dayjs';
-	import { invoke } from '@tauri-apps/api/core';
-	import { ref, watch } from 'vue';
-	
-	import { DynamicReport } from './base.ts';
-	import { db } from '../db.ts';
-	import DynamicReportComponent from '../components/DynamicReportComponent.vue';
-	import DynamicReportMenu from '../components/DynamicReportMenu.vue';
-	
-	const report = ref(null as DynamicReport | null);
-	const reportColumns = ref([] as string[]);
-	
-	const dt = ref(null as string | null);
-	const dtStart = ref(null as string | null);
-	
-	const comparePeriods = ref(1);
-	const compareUnit = ref('years');
-	
-	async function load() {
-		await db.load();
-		
-		dt.value = db.metadata.eofy_date;
-		dtStart.value = dayjs(db.metadata.eofy_date).subtract(1, 'year').add(1, 'day').format('YYYY-MM-DD');
-		
-		await updateReport();
-		
-		// Update report when dates etc. changed
-		// We initialise the watcher here only after dt and dtStart are initialised above
-		watch([dt, dtStart, comparePeriods, compareUnit], updateReport);
-	}
-	
-	async function updateReport() {
-		const dayjsDt = dayjs(dt.value!);
-		const dayjsDtStart = dayjs(dtStart.value!);
-		
-		const reportDates = [];
-		let newReportColumns = [];
-		
-		for (let i = 0; i < comparePeriods.value; i++) {
-			let thisReportDt, thisReportDtStart;
-			
-			// Get period start and end dates
-			if (compareUnit.value === 'years') {
-				thisReportDt = dayjsDt.subtract(i, 'year');
-				thisReportDtStart = dayjsDtStart.subtract(i, 'year');
-				newReportColumns.push(thisReportDt.format('YYYY'));
-			} else if (compareUnit.value === 'months') {
-				if (dayjsDt.add(1, 'day').isSame(dayjsDt.set('date', 1).add(1, 'month'))) {
-					// If dt is the end of a calendar month, then fix each prior dt to be the end of the calendar month
-					thisReportDt = dayjsDt.subtract(i, 'month').set('date', 1).add(1, 'month').subtract(1, 'day');
-					thisReportDtStart = dayjsDtStart.subtract(i, 'month');
-				} else {
-					thisReportDt = dayjsDt.subtract(i, 'month');
-					thisReportDtStart = dayjsDtStart.subtract(i, 'month');
-				}
-				newReportColumns.push(thisReportDt.format('YYYY-MM'));
-			} else {
-				throw new Error('Unexpected compareUnit');
-			}
-			
-			reportDates.push([thisReportDtStart.format('YYYY-MM-DD'), thisReportDt.format('YYYY-MM-DD')]);
-		}
-		
-		if (comparePeriods.value === 1) {
-			// Override column headers if only one column
-			newReportColumns = ['$'];
-		}
-		
-		report.value = DynamicReport.fromJSON(await invoke('get_income_statement', { dates: reportDates }));
-		reportColumns.value = newReportColumns;  // Wait until report available to update this
-	}
-	
-	load();
-	
-	function onCompareUnitChange() {
-		const dayjsDt = dayjs(dt.value!);
-		const dayjsDtStart = dayjs(dtStart.value!);
-		
+import dayjs from 'dayjs';
+import { invoke } from '@tauri-apps/api/core';
+import { ref, watch } from 'vue';
+
+import { DynamicReport } from './base.ts';
+import { db } from '../db.ts';
+import DynamicReportComponent from '../components/DynamicReportComponent.vue';
+import DynamicReportMenu from '../components/DynamicReportMenu.vue';
+
+const report = ref(null as DynamicReport | null);
+const reportColumns = ref([] as string[]);
+
+const dt = ref(null as string | null);
+const dtStart = ref(null as string | null);
+
+const comparePeriods = ref(1);
+const compareUnit = ref('years');
+
+async function load() {
+	await db.load();
+
+	dt.value = db.metadata.eofy_date;
+	dtStart.value = dayjs(db.metadata.eofy_date).subtract(1, 'year').add(1, 'day').format('YYYY-MM-DD');
+
+	await updateReport();
+
+	// Update report when dates etc. changed
+	// We initialise the watcher here only after dt and dtStart are initialised above
+	watch([dt, dtStart, comparePeriods, compareUnit], updateReport);
+}
+
+async function updateReport() {
+	const dayjsDt = dayjs(dt.value!);
+	const dayjsDtStart = dayjs(dtStart.value!);
+
+	const reportDates = [];
+	let newReportColumns = [];
+
+	for (let i = 0; i < comparePeriods.value; i++) {
+		let thisReportDt, thisReportDtStart;
+
+		// Get period start and end dates
 		if (compareUnit.value === 'years') {
-			if (dayjsDt.add(1, 'day').subtract(1, 'month').isSame(dayjsDtStart)) {
-				// Dates were previously set to one month - now compareUnit changed to years
-				// Automatically change dates to one year
-				dtStart.value = dayjsDt.add(1, 'day').subtract(1, 'year').format('YYYY-MM-DD');
-			}
+			thisReportDt = dayjsDt.subtract(i, 'year');
+			thisReportDtStart = dayjsDtStart.subtract(i, 'year');
+			newReportColumns.push(thisReportDt.format('YYYY'));
 		} else if (compareUnit.value === 'months') {
-			if (dayjsDt.add(1, 'day').subtract(1, 'year').isSame(dayjsDtStart)) {
-				// Dates were previously set to one year - now compareUnit changed to months
-				// Automatically change dates to one month
-				dtStart.value = dayjsDt.add(1, 'day').subtract(1, 'month').format('YYYY-MM-DD');
+			if (dayjsDt.add(1, 'day').isSame(dayjsDt.set('date', 1).add(1, 'month'))) {
+				// If dt is the end of a calendar month, then fix each prior dt to be the end of the calendar month
+				thisReportDt = dayjsDt.subtract(i, 'month').set('date', 1).add(1, 'month').subtract(1, 'day');
+				thisReportDtStart = dayjsDtStart.subtract(i, 'month');
+			} else {
+				thisReportDt = dayjsDt.subtract(i, 'month');
+				thisReportDtStart = dayjsDtStart.subtract(i, 'month');
 			}
+			newReportColumns.push(thisReportDt.format('YYYY-MM'));
 		} else {
 			throw new Error('Unexpected compareUnit');
 		}
+
+		reportDates.push([thisReportDtStart.format('YYYY-MM-DD'), thisReportDt.format('YYYY-MM-DD')]);
 	}
+
+	if (comparePeriods.value === 1) {
+		// Override column headers if only one column
+		newReportColumns = ['$'];
+	}
+
+	report.value = DynamicReport.fromJSON(await invoke('get_income_statement', { dates: reportDates }));
+	reportColumns.value = newReportColumns;  // Wait until report available to update this
+}
+
+load();
+
+function onCompareUnitChange() {
+	const dayjsDt = dayjs(dt.value!);
+	const dayjsDtStart = dayjs(dtStart.value!);
+
+	if (compareUnit.value === 'years') {
+		if (dayjsDt.add(1, 'day').subtract(1, 'month').isSame(dayjsDtStart)) {
+			// Dates were previously set to one month - now compareUnit changed to years
+			// Automatically change dates to one year
+			dtStart.value = dayjsDt.add(1, 'day').subtract(1, 'year').format('YYYY-MM-DD');
+		}
+	} else if (compareUnit.value === 'months') {
+		if (dayjsDt.add(1, 'day').subtract(1, 'year').isSame(dayjsDtStart)) {
+			// Dates were previously set to one year - now compareUnit changed to months
+			// Automatically change dates to one month
+			dtStart.value = dayjsDt.add(1, 'day').subtract(1, 'month').format('YYYY-MM-DD');
+		}
+	} else {
+		throw new Error('Unexpected compareUnit');
+	}
+}
 </script>
